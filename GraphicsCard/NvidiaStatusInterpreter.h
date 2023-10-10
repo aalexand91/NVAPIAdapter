3fbb4357c4bdb243de7cb79512f7f4c0ce4338cd
aalexand91 <aalexand91@gmail.com>
2023-10-09 21:11:51 -0700

    Implement more of NvidiaStatusInterpreter. Leave a TODO to add more detailed messages as project is implemented.

--- GraphicsCard/NvidiaStatusInterpreter.h
+++ GraphicsCard/NvidiaStatusInterpreter.h
@@ -8,6 +8,10 @@ namespace GraphicsCards
 {
 	namespace NvidiaStatusInterpreter 
 	{
+		/// <returns>
+		/// A more detailed message about the status of the Nvidia GPU API.
+		/// Returns a general message if the status is unhandled by this function.
+		/// </returns>
 		GRAPHICS_CARD_API std::string GetStatusMessage(NvAPI_Status status);
 	}
 }
