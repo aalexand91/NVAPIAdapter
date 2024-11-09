226ad4fce8beda50febcc278154872f8a5c8224c
aalexand91 <aalexand91@gmail.com>
2024-11-09 07:04:20 -0800

    Update NVAPIAdapter library to provide GPU bus and system type.

--- Header Files/NvidiaGpu.h
+++ Header Files/NvidiaGpu.h
@@ -1,4 +1,22 @@
-// Copyright 2024 Anthony Alexander
+// Copyright (c) 2024 Anthony Alexander
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #pragma once
 
@@ -20,6 +38,18 @@ namespace NVAPIAdapter
 		{
 			System::String^ get();
 		}
+
+		/// <summary>The GPU busy type, such as PCIe.</summary>
+		property System::String^ BusType
+		{
+			System::String^ get();
+		}
+
+		/// <summary>The GPU system type, such as Desktop or Laptop.</summary>
+		property System::String^ SystemType
+		{
+			System::String^ get();
+		}
 	};
 
 	ref class NvidiaGpu : INvidiaGpu
@@ -30,6 +60,8 @@ namespace NVAPIAdapter
 
 		virtual property System::UInt32 CoreCount { System::UInt32 get(); }
 		virtual property System::String^ Name { System::String^ get(); }
+		virtual property System::String^ SystemType { System::String^ get(); }
+		virtual property System::String^ BusType { System::String^ get(); }
 
 	private:
 		IPhysicalGpuAdapter^ m_physicalGpu;
